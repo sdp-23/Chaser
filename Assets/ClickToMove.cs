c6e953ef9a25e48370b10fd83e57d8fb255a343c
sdp-23 <sdp-23@live.com>
2016-05-16 05:34:14 -0400

    first commit

---
+++ Assets/ClickToMove.cs
@@ -0,0 +1,35 @@
+﻿// ClickToMove.cs
+using UnityEngine;
+
+[RequireComponent (typeof (NavMeshAgent))]
+public class ClickToMove : MonoBehaviour {
+	RaycastHit hitInfo = new RaycastHit();
+	NavMeshAgent agent;
+	
+	void Start () {
+		agent = GetComponent<NavMeshAgent> ();
+	}
+	void Update () {
+		/*if(Input.GetMouseButtonDown(0)) {
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			if (Physics.Raycast(ray.origin, ray.direction, out hitInfo))
+				agent.destination = hitInfo.point;
+		}*/
+		
+		float x = transform.position.x;
+		float y = transform.position.y;
+		float z = transform.position.z;
+		if (Input.GetKey(KeyCode.UpArrow)) {
+			agent.destination = new Vector3(x,y,z+1);
+		}
+		if (Input.GetKey (KeyCode.DownArrow)) {
+			agent.destination = new Vector3(x,y,z-1);
+		}
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			agent.destination = new Vector3(x-1,y,z);
+		}
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			agent.destination = new Vector3(x+1,y,z);
+		}
+	}
+}
---
+++ Assets/Scripts/Tags.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tags : MonoBehaviour {
+	public const string player = "Player";
+	public const string gameController = "GameController";
+	public const string mainCamera = "MainCamera";
+	public const string horse = "Horse";
+	public const string smallTerrain = "SmallTerrain";
+	public const string bigTerrain = "BigTerrain";
+	public const string trees = "Trees";
+	public const string fader = "Fader";
+}
