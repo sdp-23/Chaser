c44156e1cfa43439b01781b3dfd78d78e5ca1d40
sdp-23 <sdp-23@live.com>
2016-05-16 14:24:40 -0400

    added stealth

---
+++ Assets/Old/AstrellaLocomotion.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (NavMeshAgent))]
+[RequireComponent (typeof (Animator))]
+public class AstrellaLocomotion : MonoBehaviour {
+	Animator anim;
+	NavMeshAgent agent;
+	Vector2 smoothDeltaPosition = Vector2.zero;
+	Vector2 velocity = Vector2.zero;
+	public bool shouldWalk;
+	void Start ()
+	{
+		anim = GetComponent<Animator> ();
+		agent = this.GetComponent<NavMeshAgent> ();
+		// Don't update position automatically
+		agent.updatePosition = false;
+		shouldWalk = false;
+	}
+	
+	void Update ()
+	{
+		Vector3 worldDeltaPosition = agent.nextPosition - transform.position;
+		
+		// Map 'worldDeltaPosition' to local space
+		float dx = Vector3.Dot (transform.right, worldDeltaPosition);
+		float dy = Vector3.Dot (transform.forward, worldDeltaPosition);
+		Vector2 deltaPosition = new Vector2 (dx, dy);
+		
+		// Low-pass filter the deltaMove
+		float smooth = Mathf.Min(1.0f, Time.deltaTime/0.15f);
+		smoothDeltaPosition = Vector2.Lerp (smoothDeltaPosition, deltaPosition, smooth);
+		
+		// Update velocity if delta time is safe
+		if (Time.deltaTime > 1e-5f)
+			velocity = smoothDeltaPosition / Time.deltaTime;
+
+		shouldWalk = Input.GetKey (KeyCode.DownArrow) || Input.GetKey (KeyCode.UpArrow) || Input.GetKey (KeyCode.RightArrow) || Input.GetKey (KeyCode.LeftArrow);
+		bool shouldRun = velocity.magnitude > 0.7f && agent.remainingDistance > agent.radius && Input.GetKey(KeyCode.R);
+
+		// Update animation parameters
+		anim.SetBool("walk", shouldWalk);
+		anim.SetBool("run", shouldRun);
+		//anim.SetFloat ("velx", velocity.x);
+		//anim.SetFloat ("vely", velocity.y);
+		
+		LookAt lookAt = GetComponent<LookAt> ();
+		if (lookAt)
+			lookAt.lookAtTargetPosition = agent.steeringTarget + transform.forward;
+		
+		// Pull character towards agent
+		//		if (worldDeltaPosition.magnitude > agent.radius)
+		//			transform.position = agent.nextPosition - 0.9f*worldDeltaPosition;
+		
+		// Pull agent towards character
+		if (worldDeltaPosition.magnitude > agent.radius)
+			agent.nextPosition = transform.position + 0.9f*worldDeltaPosition;
+	}
+	
+	void OnAnimatorMove ()
+	{
+		// Update postion to agent position
+		transform.position = agent.nextPosition;
+		
+		//		// Update position based on animation movement using navigation surface height
+		//		Vector3 position = anim.rootPosition;
+		//	
+	}
+}
---
+++ Assets/Old/ClickToMove.cs
@@ -0,0 +1,41 @@
+﻿// ClickToMove.cs
+using UnityEngine;
+
+[RequireComponent (typeof (NavMeshAgent))]
+public class ClickToMove : MonoBehaviour {
+	RaycastHit hitInfo = new RaycastHit();
+	NavMeshAgent agent;
+	
+	void Start () {
+		agent = GetComponent<NavMeshAgent> ();
+	}
+	void Update () {
+		/*if(Input.GetMouseButtonDown(0)) {
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			if (Physics.Raycast(ray.origin, ray.direction, out hitInfo))
+				agent.destination = hitInfo.point;
+		}*/
+		
+		float x = transform.position.x;
+		float y = transform.position.y;
+		float z = transform.position.z;
+		bool walk = false;
+		if (Input.GetKey(KeyCode.UpArrow)) {
+			walk = true;
+			agent.destination = new Vector3(x,y,z+(agent.radius));
+		}
+		if (Input.GetKey (KeyCode.DownArrow)) {
+			walk = true;
+			agent.destination = new Vector3(x,y,z-(agent.radius));
+		}
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			walk = true;
+			agent.destination = new Vector3(x-(agent.radius),y,z);
+		}
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			walk = true;
+			agent.destination = new Vector3(x+(agent.radius),y,z);
+		}
+		//GetComponent<AstrellaLocomotion> ().shouldWalk = walk;
+	}
+}
