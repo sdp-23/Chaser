4734d4ce50222de39ede60b44c0e9d145ef9a8dd
Max Tatiefo <d_davy_max@yahoo.fr>
2016-05-16 23:12:20 -0400

    Added Ethan animation, Reorganized files, Fixed walking/running speed.

---
+++ Assets/Scripts/MoveEthan.cs
@@ -0,0 +1,72 @@
+﻿// ClickToMove.cs
+using UnityEngine;
+
+[RequireComponent (typeof (NavMeshAgent))]
+public class ClickToMove : MonoBehaviour {
+	RaycastHit hitInfo = new RaycastHit();
+	NavMeshAgent agent;
+
+	private float speedDiff = 1.0f;
+
+	void Start () {
+		agent = GetComponent<NavMeshAgent> ();
+	}
+	void Update () {
+		
+		float x = transform.position.x;
+		float y = transform.position.y;
+		float z = transform.position.z;
+
+		if (Input.GetKey(KeyCode.R)) {
+			speedDiff = 5.0f;
+		}
+
+		if (Input.GetKey (KeyCode.UpArrow)) {
+
+			if (Input.GetKey (KeyCode.LeftArrow)) {
+				agent.destination = new Vector3 (x - (agent.radius*speedDiff), y, z + (agent.radius*speedDiff));
+			}
+			else if (Input.GetKey (KeyCode.RightArrow)) {
+						agent.destination = new Vector3 (x + (agent.radius*speedDiff), y, z + (agent.radius*speedDiff));
+			} else {
+				agent.destination = new Vector3 (x, y, z + (agent.radius*speedDiff));
+			}
+		} 
+
+		else if (Input.GetKey (KeyCode.DownArrow)) {
+			if (Input.GetKey (KeyCode.LeftArrow)) {
+				agent.destination = new Vector3 (x - (agent.radius*speedDiff), y, z - (agent.radius*speedDiff));
+			}
+			else if (Input.GetKey (KeyCode.RightArrow)) {
+				agent.destination = new Vector3 (x + (agent.radius*speedDiff), y, z - (agent.radius*speedDiff));
+			} else {
+				agent.destination = new Vector3 (x, y, z - (agent.radius*speedDiff));
+			}
+		} 
+
+		else if (Input.GetKey (KeyCode.LeftArrow)) {
+			if (Input.GetKey (KeyCode.UpArrow)) {
+				agent.destination = new Vector3 (x - (agent.radius*speedDiff), y, z + (agent.radius*speedDiff));
+			}
+			else if (Input.GetKey (KeyCode.DownArrow)) {
+				agent.destination = new Vector3 (x - (agent.radius*speedDiff), y, z - (agent.radius*speedDiff));
+			} else {
+				agent.destination = new Vector3 (x - (agent.radius*speedDiff), y, z);
+			}
+		} 
+
+		else if (Input.GetKey (KeyCode.RightArrow)) {
+			if (Input.GetKey (KeyCode.UpArrow)) {
+				agent.destination = new Vector3 (x + (agent.radius*speedDiff), y, z + (agent.radius*speedDiff));
+			}
+			else if (Input.GetKey (KeyCode.DownArrow)) {
+				agent.destination = new Vector3 (x + (agent.radius*speedDiff), y, z - (agent.radius*speedDiff));
+			} else {
+				agent.destination = new Vector3 (x + (agent.radius*speedDiff), y, z);
+			}
+		} 
+		else {
+			agent.destination = transform.position;
+		}
+	}
+}
